fb37864278cb00c27ef9d9da30c95f2c3fa0f234
Stephen Tempelman <mario1996@live.ca>
2019-11-13 01:08:14 -0500

    version 2.0 pitch

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
+"""Module principal du jeu"""
 import argparse
 
-from api import débuter_partie, lister_parties, jouer_coup
+from api import débuter_partie, jouer_coup
 
-def main(args):
-    print(args)
 
 def analyser_commande():
+    """Permet d'utiliser la command line"""
     parser = argparse.ArgumentParser(
-    description = "Jeu Quoridor - phase 1")
-    parser.add_argument(dest='idul', type = str, help = "IDUL du joueur.")
-    parser.add_argument("-l","--lister", action="store_true", help = "Lister les identifiants de vos 20 dernières parties." )
+        description="Jeu Quoridor - phase 1")
+    parser.add_argument(dest='idul', type=str, help="IDUL du joueur.")
+    parser.add_argument("-l", "--lister",
+                        action="store_true",
+                        help="Lister les identifiants de vos 20 dernières parties.")
     args = parser.parse_args()
     return args
 
 def afficher_damier_ascii(infojeu):
+    """Permet de convertir un dictionnaire en damier ascii"""
     lignes = []
-    lignes += list("Légende: 1="+ str(infojeu["joueurs"][0]["nom"])+', 2='+str(infojeu["joueurs"][1]["nom"]) + "\n")
+    lignes += list("Légende: 1="+ str(infojeu["joueurs"][0]["nom"])+
+            ', 2='+str(infojeu["joueurs"][1]["nom"]) + "\n")
     lignes += list("   "+"-"*35+"\n")
-    for i in range(1,10):
+    for i in range(1, 10):
         lignes += str(10-i) + " | "
-        for j in range (1,9):
+        for j in range(1, 9):
             strplayer = "."
             if [j, 10-i] == infojeu["joueurs"][0]["pos"]:
                 strplayer = "1"
             elif [j, 10-i] == infojeu["joueurs"][1]["pos"]:
                 strplayer = "2"
-            if [j+1,10-i] in infojeu["murs"]["verticaux"]:
+            if [j+1, 10-i] in infojeu["murs"]["verticaux"]:
                 lignes += list(strplayer + " | ")
-            elif [j+1,9-i] in infojeu["murs"]["verticaux"]:
-                lignes += list(strplayer + " | ")    
+            elif [j+1, 9-i] in infojeu["murs"]["verticaux"]:
+                lignes += list(strplayer + " | ")
             else:
                 lignes += list(strplayer + "   ")
         if [9, 10-i] == infojeu["joueurs"][0]["pos"]:
@@ -39,24 +43,25 @@ def afficher_damier_ascii(infojeu):
             lignes += list(". |")
         if i != 9:
             lignes += list("\n  |")
-        for k in range (1,9):
+        for k in range(1,9):
             if i != 9:
-                if [k,10-i] in infojeu["murs"]["horizontaux"]:
+                if [k, 10-i] in infojeu["murs"]["horizontaux"]:
                     lignes += list("----")
-                elif [k-1,10-i] in infojeu["murs"]["horizontaux"] and [k+1,9-i] in infojeu["murs"]["verticaux"]:
+                elif [k-1, 10-i] in infojeu["murs"]["horizontaux"] and \
+                    [k+1, 9-i] in infojeu["murs"]["verticaux"]:
                     lignes += list("---|")
-                elif [k-1,10-i] in infojeu["murs"]["horizontaux"]:
+                elif [k-1, 10-i] in infojeu["murs"]["horizontaux"]:
                     lignes += list("--- ")
-                elif [k+1,9-i] in infojeu["murs"]["verticaux"]:
+                elif [k+1, 9-i] in infojeu["murs"]["verticaux"]:
                     lignes += list("   |")
                 else:
                     lignes += list("    ")
         if i != 9:
-            if [8,10-i] in infojeu["murs"]["horizontaux"]:
-                    lignes += list("---|")
-            else: 
+            if [8, 10-i] in infojeu["murs"]["horizontaux"]:
+                lignes += list("---|")
+            else:
                 lignes += list("   |")
-        lignes += list("\n")          
+        lignes += list("\n")
     lignes += list("--|"+ "-"*35+"\n")
     lignes += list("  | 1   2   3   4   5   6   7   8   9")
     lignes = ''.join(lignes)
